411b6c35f20d6d1ee34b9b3cd513084d4138b4be
zhured <407515773@qq.com>
2017-04-02 10:42:17 +0800

    test for link wechat

--- app/Http/Controllers/WechatController.php
+++ app/Http/Controllers/WechatController.php
@@ -16,46 +16,97 @@ class WechatController extends Controller
     {
 //        Log::info('request arrived.'); # 注意：Log 为 Laravel 组件，所以它记的日志去 Laravel 日志看，而不是 EasyWeChat 日志
 
-        $wechat = app('wechat');
-//        $userApi =$wechat->user;
-        $wechat->server->setMessageHandler(function($message) {
-            switch ($message->MsgType) {
-                case 'event':
-                    return '收到事件消息';
-                    break;
-                case 'text':
-                    return '你好' . $message->FromUserName;
-                    break;
-                case 'image':
-                    return '收到图片消息';
-                    break;
-                case 'voice':
-                    return '收到语音消息';
-                    break;
-                case 'video':
-                    return '收到视频消息';
-                    break;
-                case 'location':
-                    return '收到坐标消息';
-                    break;
-                case 'link':
-                    return '收到链接消息';
-                    break;
-                // ... 其它消息
-                default:
-                    return '收到其它消息';
-                    break;
-            }
-        });
+//        $wechat = app('wechat');
+////        $userApi =$wechat->user;
+//        $wechat->server->setMessageHandler(function($message) {
+//
+//        });
+
+
 
 //        Log::info('return response.');
 
-        return $wechat->server->serve();
+//        return $wechat->server->serve();
+
+        header('Content-type:text');
+        define("TOKEN", "weixin");
+        $wechatObj = new WechatController();
+        if (isset($_GET['echostr'])) {
+            $wechatObj->valid();
+        }else{
+            $wechatObj->responseMsg();
+        }
     }
+
 
     public function index()
     {
         return "index test";
 
     }
+
+
+    public function valid()
+    {
+        $echoStr = $_GET["echostr"];
+        if($this->checkSignature()){
+            header('content-type:text');
+            echo $echoStr;
+            exit;
+        }
+    }
+
+    private function checkSignature()
+    {
+        $signature = $_GET["signature"];
+        $timestamp = $_GET["timestamp"];
+        $nonce = $_GET["nonce"];
+
+        $token = TOKEN;
+        $tmpArr = array($token, $timestamp, $nonce);
+        sort($tmpArr, SORT_STRING);
+        $tmpStr = implode( $tmpArr );
+        $tmpStr = sha1( $tmpStr );
+
+        if( $tmpStr == $signature ){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    public function responseMsg()
+    {
+        $postStr = $GLOBALS["HTTP_RAW_POST_DATA"];
+
+        if (!empty($postStr)){
+            $postObj = simplexml_load_string($postStr, 'SimpleXMLElement', LIBXML_NOCDATA);
+            $fromUsername = $postObj->FromUserName;
+            $toUsername = $postObj->ToUserName;
+            $keyword = trim($postObj->Content);
+            $time = time();
+            $textTpl = "<xml>
+                        <ToUserName><![CDATA[%s]]></ToUserName>
+                        <FromUserName><![CDATA[%s]]></FromUserName>
+                        <CreateTime>%s</CreateTime>
+                        <MsgType><![CDATA[%s]]></MsgType>
+                        <Content><![CDATA[%s]]></Content>
+                        <FuncFlag>0</FuncFlag>
+                        </xml>";
+            if($keyword == "?" || $keyword == "？")
+            {
+                $msgType = "text";
+                $contentStr = date("Y-m-d H:i:s",time());
+                $resultStr = sprintf($textTpl, $fromUsername, $toUsername, $time, $msgType, $contentStr);
+                echo $resultStr;
+            }
+        }else{
+            echo "";
+            exit;
+        }
+    }
 }
+
+
+
+
