f1d7c356ea1eed94b2930289a5c60aa743377145
zhured <407515773@qq.com>
2017-04-05 10:34:53 +0800

    联通测试

--- app/Http/Controllers/WechatController.php
+++ app/Http/Controllers/WechatController.php
@@ -14,28 +14,21 @@ class WechatController extends Controller
      */
     public function serve()
     {
-//        Log::info('request arrived.'); # 注意：Log 为 Laravel 组件，所以它记的日志去 Laravel 日志看，而不是 EasyWeChat 日志
-
-//        $wechat = app('wechat');
-////        $userApi =$wechat->user;
-//        $wechat->server->setMessageHandler(function($message) {
-//
-//        });
 
 
+        $wechat = app('wechat');
 
-//        Log::info('return response.');
+        $wechat->server->setMessageHandler(function($message) {
+                return "hello This is test";
+        });
 
-//        return $wechat->server->serve();
 
-        header('Content-type:text');
-        define("TOKEN", "weixin");
-        $wechatObj = new WechatController();
-        if (isset($_GET['echostr'])) {
-            $wechatObj->valid();
-        }else{
-            $wechatObj->responseMsg();
-        }
+
+
+          return $wechat->server->serve();
+
+
+
     }
 
 
@@ -46,65 +39,6 @@ class WechatController extends Controller
     }
 
 
-    public function valid()
-    {
-        $echoStr = $_GET["echostr"];
-        if($this->checkSignature()){
-            header('content-type:text');
-            echo $echoStr;
-            exit;
-        }
-    }
-
-    private function checkSignature()
-    {
-        $signature = $_GET["signature"];
-        $timestamp = $_GET["timestamp"];
-        $nonce = $_GET["nonce"];
-
-        $token = TOKEN;
-        $tmpArr = array($token, $timestamp, $nonce);
-        sort($tmpArr, SORT_STRING);
-        $tmpStr = implode( $tmpArr );
-        $tmpStr = sha1( $tmpStr );
-
-        if( $tmpStr == $signature ){
-            return true;
-        }else{
-            return false;
-        }
-    }
-
-    public function responseMsg()
-    {
-        $postStr = $GLOBALS["HTTP_RAW_POST_DATA"];
-
-        if (!empty($postStr)){
-            $postObj = simplexml_load_string($postStr, 'SimpleXMLElement', LIBXML_NOCDATA);
-            $fromUsername = $postObj->FromUserName;
-            $toUsername = $postObj->ToUserName;
-            $keyword = trim($postObj->Content);
-            $time = time();
-            $textTpl = "<xml>
-                        <ToUserName><![CDATA[%s]]></ToUserName>
-                        <FromUserName><![CDATA[%s]]></FromUserName>
-                        <CreateTime>%s</CreateTime>
-                        <MsgType><![CDATA[%s]]></MsgType>
-                        <Content><![CDATA[%s]]></Content>
-                        <FuncFlag>0</FuncFlag>
-                        </xml>";
-            if($keyword == "?" || $keyword == "？")
-            {
-                $msgType = "text";
-                $contentStr = date("Y-m-d H:i:s",time());
-                $resultStr = sprintf($textTpl, $fromUsername, $toUsername, $time, $msgType, $contentStr);
-                echo $resultStr;
-            }
-        }else{
-            echo "";
-            exit;
-        }
-    }
 }
 
 
