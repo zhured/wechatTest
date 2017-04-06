08fdab1235bfe997f5529ae78e4ed7cb395ad5e3
zhured <407515773@qq.com>
2017-04-06 16:36:34 +0800

    lll

--- resources/views/wechat/index.blade.php
+++ resources/views/wechat/index.blade.php
@@ -1,7 +1,14 @@
 <?php
 define("TOKEN", "zhured"); //TOKEN值
 $wechatObj = new wechat();
-$wechatObj->valid();
+
+if(isset($_GET["echostr"])){
+    $wechatObj->valid();
+}else{
+    $wechatObj->responseMsg();
+}
+
+
 class wechat {
     public function valid() {
         $echoStr = $_GET["echostr"];
@@ -23,6 +30,36 @@ class wechat {
             return true;
         } else {
             return false;
+        }
+    }
+
+    public function responseMsg() {
+        //---------- 接 收 数 据 ---------- //
+        $postStr = file_get_contents("php:://input");//$GLOBALS["HTTP_RAW_POST_DATA"]; //获取POST数据
+        //用SimpleXML解析POST过来的XML数据
+        if ($postStr!=null){
+            $postObj = simplexml_load_string($postStr, 'SimpleXMLElement', LIBXML_NOCDATA);
+            $fromUsername = $postObj->FromUserName;
+            $toUsername = $postObj->ToUserName;
+            $keyword = trim($postObj->Content);
+            $time = time();
+            $textTpl = "<xml>
+                        <ToUserName><![CDATA[%s]]></ToUserName>
+                        <FromUserName><![CDATA[%s]]></FromUserName>
+                        <CreateTime>%s</CreateTime>
+                        <MsgType><![CDATA[%s]]></MsgType>
+                        <Content><![CDATA[%s]]></Content>
+                        <FuncFlag>0</FuncFlag>
+                        </xml>";
+
+                $msgType = "text";
+                $contentStr = date("Y-m-d H:i:s",time()).$keyword;
+                $resultStr = sprintf($textTpl, $fromUsername, $toUsername, $time, $msgType, $contentStr);
+                echo $resultStr;
+
+        }else{
+            echo "";
+            exit;
         }
     }
 }
